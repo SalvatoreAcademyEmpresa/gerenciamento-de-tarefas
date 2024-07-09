1ca3a4115d49434067f31152a0976f2006d23c78
F-K-C <fakocarvalho@gmail.com>
2024-07-09 19:17:47 +0000

    Conexão com o MongoDB implementada

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const { MongoClient } = require('mongodb')
+
 
 const dbUrl = 'mongodb+srv://admin:AlanFabsEstudoSalvatore24@cluster0.hmmuzy9.mongodb.net'
 const dbName = 'mongo_todo_list'
 
-const app = express()
+async function main() {
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+    const Client = new MongoClient(dbUrl)
+    console.log("Conectando ao banco de dados")
+    await Client.connect()
+    console.log("Banco de dados conectado com sucesso!")
 
-app.listen(3000)
+    const db = Client.db(dbName)
+    const collection = db.collection('tarefas')
+
+    const app = express()
+
+    app.get('/', function (req, res) {
+        res.send('Hello World')
+    })
+
+    app.listen(3000)
+}
+
+main()
