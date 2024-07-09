f1237435d08548f43dc3cfb98cbfc9be88c562de
F-K-C <fakocarvalho@gmail.com>
2024-07-09 19:04:06 +0000

    Criação de variáveis de MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
 const express = require('express')
+
+const dbUrl = 'mongodb+srv://admin:AlanFabsEstudoSalvatore24@cluster0.hmmuzy9.mongodb.net'
+const dbName = 'mongo_todo_list'
+
 const app = express()
 
 app.get('/', function (req, res) {
