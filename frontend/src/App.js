afa97c569824df1affd46f3c93fa9103cab5bb67
alanrochagomes <alanrochaarg2001@gmail.com>
2024-07-10 17:33:34 -0300

    Alterando importações

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import "./App.css";
-import TodoList from "./TodoList";
+import "./css/App.css";
+import TodoList from "./components/TodoList";
+import Header from "./layout/Header";
 
 function App() {
   return (
     <div className="App">
-      <TodoList /> {/* Componente principal da sua aplicação */}
+      <TodoList /> {/* Componente principal da aplicação */}
+      <Header />
     </div>
   );
 }
---
+++ frontend/src/components/TaskForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "../css/TaskForm.css"; // Import the CSS file for TaskForm
+
+const TaskForm = ({ onAdd }) => {
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = () => {
+    if (newTodo.trim()) {
+      onAdd(newTodo);
+      setNewTodo("");
+    }
+  };
+
+  return (
+    <div className="todo-input-container">
+      <div className="todo-checkbox"></div>
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="Add new"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      {newTodo && (
+        <button className="add-button" onClick={addTodo}>
+          Add
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default TaskForm;
