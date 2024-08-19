6984d9cdafc06e3f3252eda81c339db1c91a75de
F-K-C <fakocarvalho@gmail.com>
2024-08-19 17:29:35 +0000

    Ajuste na mensagem da validação

--- backend/tasks/tasks.validation.js
+++ backend/tasks/tasks.validation.js
@@ -4,7 +4,7 @@ const createTaskSchema = Joi.object({
     title: Joi.string().min(3).required().messages({
         'string.empty': 'O título não pode estar vazio.',
         'string.min': 'O título deve ter pelo menos 3 caracteres.',
-        'any.required': 'O título é obrigatório.',
+        'any.required': 'O título é obrigatório.!',
         'string.base': 'O título deve ser uma string.'
     }),
     description: Joi.string().required().messages({
