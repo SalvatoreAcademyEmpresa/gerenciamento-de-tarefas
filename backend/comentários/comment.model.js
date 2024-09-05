c46845d207384e0f3361966328dae249db0f9f26
F-K-C <fakocarvalho@gmail.com>
2024-09-05 18:39:10 +0000

    Subindo arquivos

--- backend/comentários/comment.model.js
+++ backend/comentários/comment.model.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
-const commentSchema = new mongoose.Schema({
-    content: { type: String, required: true },
-    author: { type: String, required: true },
-    postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-    createdAt: { type: Date, default: Date.now},
-});
+const commentSchema = new mongoose.Schema(
+    {
+        content: { type: String, required: true },
+        author: { type: String, required: true },
+        postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+        createdAt: { type: Date, default: Date.now },
+    }
+);
 
 const Comment = mongoose.model('Comment', commentSchema);
 
--- backend/comentários/comment.router.js
+++ backend/comentários/comment.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const commentController = require('./comment.controller');
 
-const router = express.router();
+const router = express.Router();
 
 router.post('/', commentController.createComment);
 router.get('/post/:postId', commentController.getCommentsByPostId);
