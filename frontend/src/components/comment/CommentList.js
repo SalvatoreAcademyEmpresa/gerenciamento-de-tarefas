e7005f5fd2a86197d57b616c90c2d9c7f635dad8
alanrochagomes <alanrochaarg2001@gmail.com>
2024-09-26 18:20:16 -0300

    add:  Description para tarefas

--- frontend/src/components/comment/CommentList.js
+++ frontend/src/components/comment/CommentList.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes, faComment } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import "../comment/css/CommentList.css";
 import CommentBox from "./CommentBox";
 
@@ -9,55 +9,48 @@ const CommentList = ({ task, onClose }) => {
   const [isCommentBoxVisible, setIsCommentBoxVisible] = useState(false);
   const [activeCommentIndex, setActiveCommentIndex] = useState(null);
 
-  const users = ["Alice", "Bob", "Charlie"];
+  const user = "User";
 
-  const getRandomUser = () => users[Math.floor(Math.random() * users.length)];
+  const hasDescriptionOrComment =
+    !!task.description || comments.some((comment) => comment.user === user);
 
   const handleAddComment = (comment, index) => {
-    const newCommentObject = {
-      text: comment,
-      user: comments.length === 0 ? "Jordan" : getRandomUser(),
-      replies: [],
-    };
+    let updatedComments = [...comments];
 
-    let updatedComments;
     if (index !== null) {
-      updatedComments = [...comments];
-      updatedComments[index].replies.push(newCommentObject);
+      updatedComments[index].text = comment;
     } else {
-      updatedComments = [...comments, newCommentObject];
+      const newCommentObject = {
+        text: comment,
+        user: user,
+        replies: [],
+      };
+      updatedComments.push(newCommentObject);
     }
 
     setComments(updatedComments);
     task.comments = updatedComments;
   };
 
-  const handleReply = (index) => {
-    setIsCommentBoxVisible(true);
+  const handleEditComment = (index) => {
     setActiveCommentIndex(index);
+    setIsCommentBoxVisible(true);
   };
 
-  const openGoogleReminder = () => {
-    const url = "https://calendar.google.com/calendar/u/0/r";
-    window.location.href = url;
+  const handleAddDescription = () => {
+    setActiveCommentIndex(null);
+    setIsCommentBoxVisible(true);
   };
 
   return (
     <div className="task-details">
-      <div className="task-details-header">
-        <div className="task-details-header-content">
-          <h3>{task.title}</h3>
-        </div>
-
-        <div className="task-details-icons">
-          <FontAwesomeIcon
-            icon={faTimes}
-            className="task-details-icon"
-            onClick={onClose}
-          />
-        </div>
+      <div className="task-details-icons">
+        <FontAwesomeIcon
+          icon={faTimes}
+          className="task-details-icon"
+          onClick={onClose}
+        />
       </div>
-
       <div className="task-details-content">
         <p>{task.description}</p>
       </div>
@@ -65,53 +58,33 @@ const CommentList = ({ task, onClose }) => {
       <div className="comments-list">
         {comments.map((comment, index) => (
           <div key={index} className="comment-container">
-            <div className="comment" onClick={() => handleReply(index)}>
+            <div className="comment" onClick={() => handleEditComment(index)}>
               <div className="comment-user">
                 <span className="user-icon">{comment.user[0]}</span>
                 <span className="user-name">{comment.user}</span>
               </div>
-
               <div className="comment-text">{comment.text}</div>
             </div>
-
-            {comment.replies && comment.replies.length > 0 && (
-              <div className="replies-list">
-                {comment.replies.map((reply, replyIndex) => (
-                  <div key={replyIndex} className="comment reply">
-                    <div className="comment-user">
-                      <span className="user-icon">{reply.user[0]}</span>
-                      <span className="user-name">{reply.user}</span>
-                    </div>
-
-                    <div className="comment-text">{reply.text}</div>
-                  </div>
-                ))}
-              </div>
-            )}
           </div>
         ))}
       </div>
 
-      <div className="task-details-actions">
-        <button className="add-reminder" onClick={openGoogleReminder}>
-          Add reminder
-        </button>
-
-        <FontAwesomeIcon
-          icon={faComment}
-          className="task-details-icon"
-          onClick={() => {
-            setActiveCommentIndex(null);
-            setIsCommentBoxVisible(true);
-          }}
-        />
-      </div>
+      {!hasDescriptionOrComment && (
+        <div className="task-details-actions">
+          <button className="add-comment" onClick={handleAddDescription}>
+            Description
+          </button>
+        </div>
+      )}
 
       {isCommentBoxVisible && (
         <CommentBox
           onAddComment={handleAddComment}
           index={activeCommentIndex}
           onClose={() => setIsCommentBoxVisible(false)}
+          existingComment={
+            activeCommentIndex !== null ? comments[activeCommentIndex].text : ""
+          }
         />
       )}
     </div>
