5b25f14bccf1c74bba69b34138e09816aa79ba76
Alan Rocha <alanrochaarg2001@gmail.com>
2024-10-18 12:24:53 -0300

    feat: implementação de modo [button off e on]

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../css/Header.css";
 import menuIcon from "../assets/img/menu.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -14,9 +15,22 @@ import {
 
 const Header = () => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [isOfflineMode, setIsOfflineMode] = useState(false);
+  const navigate = useNavigate();
 
   const toggleMenu = () => {
     setIsExpanded(!isExpanded);
+  };
+
+  const toggleOfflineMode = () => {
+    setIsOfflineMode(!isOfflineMode);
+    if (!isOfflineMode) {
+      alert("Você está agora no modo offline.");
+      navigate("/offline");
+    } else {
+      alert("Modo offline desativado.");
+      navigate("/");
+    }
   };
 
   return (
@@ -25,59 +39,52 @@ const Header = () => {
         <button className="menu-toggle" onClick={toggleMenu}>
           <img src={menuIcon} alt="Menu Icon" className="menu-icon" />
         </button>
-
-        <div className="header-title"> Futuristic Todo List | v1.0.0</div>
+        <div className="header-title">Futuristic Todo List | v1.0.0</div>
       </nav>
-
       <aside className={`side-menu ${isExpanded ? "expanded" : ""}`}>
         <div className="user-profile">
           <FontAwesomeIcon icon={faUserCircle} size="3x" />
           <span className="user-name">User</span>
         </div>
-
         <ul className="menu-items">
+          <li>
+            <button className="offline-mode-button" onClick={toggleOfflineMode}>
+              {isOfflineMode ? (
+                <span>Voltar a Ficar Online</span>
+              ) : (
+                <span>Ativar Modo Offline</span>
+              )}
+            </button>
+          </li>
           <li>
             <button className="add-task-button">
               <FontAwesomeIcon icon={faInbox} /> Add task
             </button>
           </li>
-
           <li>
             <FontAwesomeIcon icon={faSearch} />
             <span>Search</span>
           </li>
-
           <li>
             <FontAwesomeIcon icon={faInbox} />
             <span>Inbox</span>
           </li>
-
           <li>
             <FontAwesomeIcon icon={faCalendarDay} />
             <span>Reminder</span>
           </li>
-
           <li>
             <FontAwesomeIcon icon={faTrashCan} />
             <span>Bin</span>
           </li>
-
-          {/* TODO: <li>
-            <FontAwesomeIcon icon={faTags} />
-            <span>Filters & Labels</span>
-          </li> */}
         </ul>
-
-        {/* TODO: <div className="projects-section">
-          <h3>My Projects</h3>
-          <ul className="projects-list"></ul>
-        </div> */}
-
-        <footer className="mini-footer">
-          <a href="https://github.com/SalvatoreAcademyEmpresa/gerenciamento-de-tarefas">
-            <span>© 2024 Futuristic Todo List.</span>
-          </a>
-        </footer>
+        <li>
+          <footer className="mini-footer">
+            <a href="https://github.com/SalvatoreAcademyEmpresa/gerenciamento-de-tarefas">
+              <span>© 2024 Futuristic Todo List.</span>
+            </a>
+          </footer>
+        </li>
       </aside>
     </div>
   );
