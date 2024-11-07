7051b5bee2b79f3dd80ad830be0c7d9b1ddb3387
Alan Rocha <alanrochaarg2001@gmail.com>
2024-11-07 13:57:59 -0300

    fix: layout

--- frontend/src/components/TaskForm.js
+++ frontend/src/components/TaskForm.js
@@ -3,34 +3,47 @@ import React, { useState } from "react";
 const TaskForm = ({ onAdd }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [isAddingDescription, setIsAddingDescription] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (title && description) {
-      onAdd({ title, description });
-      setTitle("");
-      setDescription("");
+    if (!isAddingDescription) {
+      if (title.trim()) {
+        setIsAddingDescription(true);
+      } else {
+        alert("Por favor, preencha o título.");
+      }
     } else {
-      alert("Por favor, preencha todos os campos.");
+      if (description.trim()) {
+        onAdd({ title, description });
+        setTitle("");
+        setDescription("");
+        setIsAddingDescription(false);
+      } else {
+        alert("Por favor, preencha a descrição.");
+      }
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        required
-      />
+      {!isAddingDescription ? (
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+      ) : (
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+      )}
       <button type="submit">➕</button>
     </form>
   );
--- frontend/src/components/TaskItem.js
+++ frontend/src/components/TaskItem.js
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
 import "../css/TaskItem.css";
 import moveIcon from "../assets/img/move-icon.svg";
-import Reminder from "./reminder/Reminder";
+import chatIcon from "../assets/img/chat.svg";
+import Reminder from "../components/reminder/Reminder";
 
 const TaskItem = ({
   task,
   onEdit,
-  onStartEdit,
   onDelete,
+  onMoveTask,
+  onToggleCompletion,
   isEditing,
   isCompleted,
-  onToggleCompletion,
+  onStartEdit,
+  onReminder,
 }) => {
   const [title, setTitle] = useState(task.title);
   const [description, setDescription] = useState(task.description);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
+  const [showDescription, setShowDescription] = useState(false);
 
   const handleSaveEdit = () => {
     onEdit({ title, description });
@@ -34,9 +38,27 @@ const TaskItem = ({
     setShowConfirmModal(false);
   };
 
+  const handleDragStart = (event) => {
+    event.dataTransfer.setData("text", task.id);
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const droppedTaskId = event.dataTransfer.getData("text");
+    onMoveTask(droppedTaskId, task.id);
+  };
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription);
+  };
+
   return (
     <div
       className="task-item-list"
+      draggable
+      onDragStart={handleDragStart}
+      onDrop={handleDrop}
+      onDragOver={(event) => event.preventDefault()}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -68,28 +90,46 @@ const TaskItem = ({
           <h3 style={{ textDecoration: isCompleted ? "line-through" : "none" }}>
             {task.title}
           </h3>
-          <div className="description-hover">
-            <p>{task.description}</p>
-          </div>
+          {showDescription && <p>{task.description}</p>}
           <div
             className="icon-buttons"
             style={{ display: isHovered ? "flex" : "none" }}
           >
             <button onClick={onStartEdit} className="icon-edit-button-list">
-              ✏️
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                width="1rem"
+                height="1rem"
+              >
+                <path d="M4 21h16v-2H4v2zm3.5-4.5l1.09-1.09 7.09 7.09L14.5 21H4v-2h3.5v-2.5zm9.91-9.91l-1.41-1.41 2.83-2.83 1.41 1.41-2.83 2.83zm-1.41 1.41L7.5 18.5V21h2.5l8.5-8.5-2.5-2.5z" />
+              </svg>
             </button>
             <button
               onClick={handleDeleteClick}
               className="icon-delete-button-list"
             >
-              🗑️
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                width="1rem"
+                height="1rem"
+              >
+                <path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-4.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z" />
+              </svg>
             </button>
-
+            <img
+              src={chatIcon}
+              alt="Chat icon"
+              className="chat-icon"
+              onClick={toggleDescription}
+            />
             <Reminder />
           </div>
         </div>
       )}
-
       {showConfirmModal && (
         <div className="confirmation-modal">
           <div className="modal-content">
