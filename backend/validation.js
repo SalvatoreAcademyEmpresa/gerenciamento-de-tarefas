a7c9713e198d23bfad11061a6e4c9c2370400c3a
Alan Rocha <alanrochaarg2001@gmail.com>
2024-12-09 09:39:58 -0300

    chore: implementando `taskOrderSchema`

--- backend/validation.js
+++ backend/validation.js
@@ -1,10 +1,18 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const taskSchema = Joi.object({
-    title: Joi.string().required(),
-    description: Joi.string().required()
+  title: Joi.string().required(),
+  description: Joi.string().required(),
 });
 
+const taskOrderSchema = Joi.array().items(
+  Joi.object({
+    id: Joi.string().required(),
+    order: Joi.number().required(),
+  })
+);
+
 module.exports = {
-    taskSchema
+  taskSchema,
+  taskOrderSchema,
 };
